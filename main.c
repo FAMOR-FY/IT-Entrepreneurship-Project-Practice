2901b4a092cfcf1fab864e4b5e87cde5d13d50ea
FAMOR-FY <57088240+FAMOR-FY@users.noreply.github.com>
2021-12-16 16:30:05 +0800

    v1.1.0

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+// 配置头文件
 #include "config.h"
+// 输入输出头文件
 #include "ioput.h"
+// 操作提示头文件
 #include "ui.h"
+// 计算器程序头文件
 #include "calculator.h"
 
 int main(){
@@ -11,20 +15,29 @@ int main(){
     double num2;
     int type;
     double out;
+    // 欢迎
     welcome();
     while(1){
+        // 获取运算类型
         type=get_type();
+        // 获取输入
         num_array = input();
         num1 = num_array[0];
         num2 = num_array[1];
+        // 判断格式是否合法
         if(is_legal(num1,num2,type)){
+            // 计算
             out=calculator(num1, num2, type);
+            // 输出结果
             output(num1, num2, out, type);
         }
+        // 判断是否继续预算：1/0
         if(is_again()==0)
             break;
     };
+    // 告别
     bye();
+    // 暂停，防止窗体直接关闭
     system("PAUSE");
     return 0;
 }
